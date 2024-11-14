8d66e42191911a4f1ffc6d9366007134ce409f8f
ebentarigan <ebenezer0852@gmail.com>
2024-11-14 14:04:29 +0700

    nambah create product

--- praktikum7/create_product.php
+++ praktikum7/create_product.php
@@ -1,6 +1,20 @@
-<?php
-include 'db_connection'
+<?
+include 'db_connection';
 
+if ($_SERVER['REQUEST_METHOD'] === 'POST' && $_POST['submit']) {
+    $nama_produk = $_POST['nama_produk'];
+    $harga= $_POST['harga'];
+    $stok = $_POST['stok'];
+
+    $sql = "INSERT INTO products (nama_produk, harga, stok) VALUES
+    ($nama_produk, $harga, $stok)";
+
+    if ( $conn -> query($sql) === true) {
+        echo "Produk  berhasil ditambahkan";
+    } else {
+        echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+    }
+}
 ?>
 
 
@@ -12,20 +26,19 @@ include 'db_connection'
     <title>Document</title>
 </head>
 <body>
-    <h1>Tambah produk</h1>
+    <h1>Tambah Produk</h1>
     <form action="" method="post">
-        <label for = "nama_produk">Nama produk</label>
-        <input type="text"  id="nama_produk"  name="nama_produk" required>
-        <br></br>
-        <label for = "harga_produk">Harga produk</label>
-        <input type="text"  id="nama_produk"  name="nama_produk" required>
-        <br></br>
-        <label for = "nama_produk">Nama produk</label>
-        <input type="text"  id="nama_produk"  name="nama_produk" required>
-        <br></br>
-        <label for = "nama_produk">Nama produk</label>
-        <input type="text"  id="nama_produk"  name="nama_produk" required>
-        <br></br>
+        <label for="nama_produk">Nama Produk: </label>
+        <input type="text" id="nama_produk" name="nama_produk" required>
+        <br><br>
+        <label for="harga_produk">Harga Produk</label>
+        <input type="number" id="harga" name="harga" required>
+        <label for="stok">Stok</label>
+        <input type="number" id="stok" name="stok" required>
+        <br><br>
+        <input type="submit" name="submit" value="Tambah Produk">
 
+
+    </form>
 </body>
 </html>
